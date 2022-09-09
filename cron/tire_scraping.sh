e6818ea22f8348db09b63ab663c00ce4bb2ce430
r.ito <>
2022-09-09 11:38:31 +0900

    aaa

--- cron/tire_scraping.sh
+++ cron/tire_scraping.sh
@@ -1,3 +1,3 @@
 cd $HOME/app/tiretrend/project
 # cron経由で実行するとphpのパスが通らいのでフルパス指定
-/usr/local/bin/php74cli artisan command:tire-scraping
+/usr/local/bin/php74cli artisan command:tire-scraping > /dev/null 2>&1
