1ee2021ebc9be2ef9872357b2d813ab8aedb6a72
ryoji <ryoji@unko.com>
2023-03-29 18:19:21 +0900

    コメント化解除

--- cron/tire_scraping.sh
+++ cron/tire_scraping.sh
@@ -1,3 +1,3 @@
 cd $HOME/tiretrend/project
 # cron経由で実行するとphpのパスが通らいのでフルパス指定
-# /usr/bin/php7.4 artisan command:tire-scraping > /dev/null 2>&1
+/usr/bin/php7.4 artisan command:tire-scraping > /dev/null 2>&1
